19eaf6387c0da5f79294d30f84a070d27f4a6ae9
Asheer Rizvi <asheer.vil@gmail.com>
2020-06-10 14:40:45 +0530

    Added a fullscreen button for the video player.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,55 +1,62 @@
-/* Get our elements */
+/* Getting the elements */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
+const ranges = player.querySelectorAll('.player__slider');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider')
+const fullscreen = player.querySelector('.fullscreen');
 
-/* Build our functions */
+/* Setup the functions */
 function togglePlay() {
     video.paused ? video.play() : video.pause();
 }
 
-function updateButton() {
-    toggle.textContent = this.paused ? '►' : '❚❚';
+function toggleFullscreen() {
+    document.fullscreenElement ? document.exitFullscreen : video.requestFullscreen();
 }
 
-function skip() {
-    console.log(this.dataset.skip);
+function updateButton() {
+    const button = video.paused ? '►' : '❚❚';
+    toggle.textContent = button;
+}
+
+function updateTime() {
+    const percentage = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percentage}%`;
+}
+
+function handleRanges() {
+    video[this.name] = this.value;
+}
+
+function handleSkip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function handleRangeUpdate() {
-    video[this.name] = this.value;
-    // console.log(this.name);
-    // console.log(this.value);
-}
-
-function handleProgress() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexBasis = `${percent}%`;
-}
 
 function scrub(e) {
     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
 }
 
-/* Hook up the event listeners */
+/* Setup the event handlers */
 video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
-video.addEventListener('timeupdate', handleProgress);
+video.addEventListener('play', updateButton);
+video.addEventListener('timeupdate', updateTime);
 
 toggle.addEventListener('click', togglePlay);
-skipButtons.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+fullscreen.addEventListener('click', toggleFullscreen);
 
-let mouseDown = false;
+ranges.forEach(range => range.addEventListener('change', handleRanges));
+ranges.forEach(range => range.addEventListener('mousemove', handleRanges));
+
+skipButtons.forEach(skipButton => skipButton.addEventListener('click', handleSkip));
+
+let mousedown = false;
 progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
-progress.addEventListener('mousedown', () => mouseDown = true);
-progress.addEventListener('mouseup', () => mouseDown = false);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
